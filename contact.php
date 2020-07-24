9fa4c3620d20bf012ce4d1f60a86f55b3cc47fe4
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2020-07-24 00:56:19 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -27,9 +27,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
-    <link rel="stylesheet" href="style.css" media="all">
+    <link rel="stylesheet" href="css/style.css" media="all">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="main.js"></script>
+    <script src="js/main.js"></script>
 </head>
 
 <body>
