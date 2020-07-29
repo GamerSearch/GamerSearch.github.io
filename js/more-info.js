79fc76634e98ba2fc36148fde9242d53a40a9d84
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2020-07-28 23:29:25 -0700

    Update more-info.js

--- js/more-info.js
+++ js/more-info.js
@@ -10,8 +10,8 @@ function drawMoreInfoPage(gameInfo) {
               <li class="list-group-item"><strong>Rating:</strong> ${gameInfo.metacritic}</li>
               <li class="list-group-item"><strong>Released:</strong> ${gameInfo.released}</li>
               <li class="list-group-item"><strong>Stores:</strong> ${gameInfo.stores}</li>
-              <li class="list-group-item"><strong>Platforms:</strong> ${gameInfo.platforms[0].name}, ${gameInfo.platforms[1].name}, ${gameInfo.platforms[2].name}</li>
-              <li class="list-group-item"><strong>Genre:</strong> ${gameInfo.genres[0].name}, ${gameInfo.genres[1].name}, ${gameInfo.genres[2].name}</li>
+              <li class="list-group-item"><strong>Platforms:</strong> ${gameInfo.platforms[0].name}, ${gameInfo.platforms[1]}, ${gameInfo.platforms[2]}</li>
+              <li class="list-group-item"><strong>Genre:</strong> ${gameInfo.genres[0]}, ${gameInfo.genres[1]}, ${gameInfo.genres[2]}</li>
           </ul>
       </div>
   </div>
