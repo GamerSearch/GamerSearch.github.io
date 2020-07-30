a4be76ee05825cbb9a51ef2b8195c1ebe7fc0708
EnliveRenaud <30477080+EnliveRenaud@users.noreply.github.com>
2020-07-29 21:20:12 -0700

    FIX: finish more-info page

--- js/more-info.js
+++ js/more-info.js
@@ -1,37 +1,59 @@
 function drawMoreInfoPage(gameInfo) {
-    let output = `
-      <div class="col-md-4>
-          <img src="https://media.rawg.io/media/games/${gameInfo.background_image}" class=thumbail" alt="image>
-      </div>
+  var allStores = [];
+  var stores = gameInfo['stores'];
+  stores.forEach(function(item) {
+    allStores.push(item["store"]["name"]);
+  })
+  var storesString = allStores.join(", ");
 
+  var allPlatforms = [];
+  var platforms = gameInfo['parent_platforms'];
+  platforms.forEach(function(item) {
+    allPlatforms.push(item["platform"]["name"]);
+  })
+  var platFormString = allPlatforms.join(", ");
+
+  var allGenres = [];
+  var genres = gameInfo['genres'];
+  genres.forEach(function(item) {
+    allGenres.push(item["name"]);
+  })
+  var genresString = allGenres.join(", ");
+
+  var allDevelopers = [];
+  var developers = gameInfo['developers'];
+  developers.forEach(function(item) {
+    allDevelopers.push(item["name"]);
+  })
+  var developersString = allDevelopers.join(", ");
+
+  let output = `
       <div class="col-md-8">
           <h2>${gameInfo.name}</h2>
+          <img src="${gameInfo.background_image}" class="img-responsive">
           <ul class="list-group">
-              <li class="list-group-item"><strong>Rating:</strong> ${gameInfo.metacritic}</li>
-              <li class="list-group-item"><strong>Released:</strong> ${gameInfo.released}</li>
-              <li class="list-group-item"><strong>Stores:</strong> ${gameInfo.stores[0]}</li>
-              <li class="list-group-item"><strong>Platforms:</strong> ${gameInfo.platforms[0].platform.name}</li>
-              <li class="list-group-item"><strong>Genre:</strong> ${gameInfo.genres[0].name}</li>
-              <li class="list-group-item"><strong>Developer:</strong> ${gameInfo.developers[0].name}</li>
+              <li class="list-group-item" id="rating"><strong>Metacritic Rating: </strong>${gameInfo.metacritic}</li>
+              <li class="list-group-item" id="releaseDate"><strong>Released: </strong>${gameInfo.released}</li>
+              <li class="list-group-item" id="stores"><strong>Store(s): </strong>${storesString}</li>
+              <li class="list-group-item" id="platforms"><strong>Platform(s): </strong>${platFormString}</li>
+              <li class="list-group-item" id="genres"><strong>Genre(s): </strong>${genresString}</li>
+              <li class="list-group-item" id="developers"><strong>Developer(s): </strong>${developersString}</li>
             </ul>
       </div>
-  </div>
 
-  <div class="row">
-      <div class="well">
-          <h3>Description</h3>
-          ${gameInfo.description}
-          <hr>
-          <a href="index.html" class="btn btn-default">Go Back to Search</a>
+      <div class="row">
+        <h3>Description</h3>
+        ${gameInfo.description}
+        <a href="index.html" class="btn btn-default">Go Back to Search</a>
       </div>
-  </div>
   `;
   $('#game').html(output);
 }
 
 function init() {
   var gameInfo = JSON.parse(sessionStorage.getItem('gameInfo'));
-  drawMoreInfoPage(gameInfo );
+  drawMoreInfoPage(gameInfo);
+
 }
 
 init();
