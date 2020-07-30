0ad9fc7164e71b904b3056f0b65fc1e34f35223d
Renaud Roques <30477080+RenaudRoques@users.noreply.github.com>
2020-07-30 10:12:06 -0700

    FIX: streamlined function to get stores, platforms, genres and devs

--- js/more-info.js
+++ js/more-info.js
@@ -1,32 +1,15 @@
+function getInfoFromKey(allValues, additionalAccessor) {
+  var values = [];
+  allValues.forEach(function (item) {
+    if (additionalAccessor) { values.push(item[additionalAccessor]['name']); }
+    else { values.push(item['name']); }
+  });
+
+  return values.join(", ");
+}
+
+
 function drawMoreInfoPage(gameInfo) {
-  var allStores = [];
-  var stores = gameInfo['stores'];
-  stores.forEach(function(item) {
-    allStores.push(item["store"]["name"]);
-  })
-  var storesString = allStores.join(", ");
-
-  var allPlatforms = [];
-  var platforms = gameInfo['parent_platforms'];
-  platforms.forEach(function(item) {
-    allPlatforms.push(item["platform"]["name"]);
-  })
-  var platFormString = allPlatforms.join(", ");
-
-  var allGenres = [];
-  var genres = gameInfo['genres'];
-  genres.forEach(function(item) {
-    allGenres.push(item["name"]);
-  })
-  var genresString = allGenres.join(", ");
-
-  var allDevelopers = [];
-  var developers = gameInfo['developers'];
-  developers.forEach(function(item) {
-    allDevelopers.push(item["name"]);
-  })
-  var developersString = allDevelopers.join(", ");
-
   let output = `
       <div class="col-md-8">
           <h2>${gameInfo.name}</h2>
@@ -34,10 +17,10 @@ function drawMoreInfoPage(gameInfo) {
           <ul class="list-group">
               <li class="list-group-item" id="rating"><strong>Metacritic Rating: </strong>${gameInfo.metacritic}</li>
               <li class="list-group-item" id="releaseDate"><strong>Released: </strong>${gameInfo.released}</li>
-              <li class="list-group-item" id="stores"><strong>Store(s): </strong>${storesString}</li>
-              <li class="list-group-item" id="platforms"><strong>Platform(s): </strong>${platFormString}</li>
-              <li class="list-group-item" id="genres"><strong>Genre(s): </strong>${genresString}</li>
-              <li class="list-group-item" id="developers"><strong>Developer(s): </strong>${developersString}</li>
+              <li class="list-group-item" id="stores"><strong>Store(s): </strong>${getInfoFromKey(gameInfo['stores'], 'store')}</li>
+              <li class="list-group-item" id="platforms"><strong>Platform(s): </strong>${getInfoFromKey(gameInfo['parent_platforms'], 'platform')}</li>
+              <li class="list-group-item" id="genres"><strong>Genre(s): </strong>${getInfoFromKey(gameInfo['genres'], false)}</li>
+              <li class="list-group-item" id="developers"><strong>Developer(s): </strong>${getInfoFromKey(gameInfo['developers'], false)}</li>
             </ul>
       </div>
 
